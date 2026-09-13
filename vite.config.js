57f5c12edc91848995d5ee1adcf9301dcaee7c1e
sampadaambavale97 <sampadaambavale97@gmail.com>
2026-09-13 13:18:23 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss()],base:"/
+    Task-Tracker-App-React-Project-/",
 });
